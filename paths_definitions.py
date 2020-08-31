61b5509fd0458333dcdc5365c2d2d9b761710914
thibaut1998e <thibaut.eloy@imt-atlantique.net>
2020-08-31 11:14:37 +0200

    remove unused variable

--- paths_definitions.py
+++ paths_definitions.py
@@ -1,9 +1,10 @@
 
 
 myHome = "/home/eloy"
-metrics_folder = f'{myHome}/metrics_record' #json file containing losses and metrics are saved here
+
 training_sets = myHome + "/training_sets"
 models = myHome + "/models" #location in which models architecture are saved
+
 
 
 
